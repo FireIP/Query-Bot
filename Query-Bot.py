77ca40b08497ce03ea9b32482b11df589a24db47
Felix <felix.anderka@gmail.com>
2022-05-01 14:55:21 +0200

    v0.8 #	complete rewrite #	handeled offline servers #	added aditional commands available via Discord

--- Query-Bot.py
+++ Query-Bot.py
@@ -1,151 +1,183 @@
-
-#Ein Discord-Bot um den Status eines Minecraft servers und die Spieler-Liste in einen discord chat zu schreiben.
+# Ein Discord-Bot um den Status eines Minecraft servers und die Spieler-Liste in einen discord chat zu schreiben.
+# 
+# @author (FireIP) 
+# @version (0.8)
 #
-# @author (FireIP)
-# @version (0.7.1)
 #
-#
-#	message when bot comes online
-
+#	complete rewrite
+#	handeled offline servers
+#	added aditional commands available via Discord
 
 
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
 
 import asyncio
 import time
 
 from threading import Thread
+
 from mcstatus import MinecraftServer
+
+global _loop
+_loop = None
+
+global SSchannel
+SSchannel = None
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    global _loop
+    global SSchannel
+
+    SSchannel = client.get_channel(563437211459256331)
+
+    await SSchannel.send("Bot is online")
+    print("Bot is online and connected to Discord")
+
+    _loop = asyncio.get_event_loop()
+
+    qt.start()
+
+
+@client.event
+async def on_message(message):
+    global SSchannel
+    global q
+    global sOnline
+    global lastQuery
+
+    if message.content == "cookie":
+        await message.channel.send(":cookie:")
+
+    elif message.content.startswith("s-"):
+
+        if message.content == "s-stopQuery":
+            if message.author.id == "000000000000000000":   #replace with discord user ID of admin
+                q = False
+
+                print("Query stoped!")
+                await message.channel.send("Query stoped!")
+
+        elif message.content == "s-startQuery":
+            if message.author.id == "000000000000000000":   #replace with discord user ID of admin
+                if q == False:
+                    q = True
+
+                    qt = Thread(target=queryThread)
+                    qt.start()
+
+                print("Query started!")
+                await message.channel.send("Query started!")
+
+        elif message.content == "s-restartQuery":
+            if message.author.id == "000000000000000000":   #replace with discord user ID of admin
+                q = False
+
+                await message.channel.send("Restarting Query...")
+
+                time.sleep(31)
+
+                print("Query stoped!")
+                await message.channel.send("Query stoped!")
+
+                q = True
+
+                qt = Thread(target=queryThread)
+                qt.start()
+                time.sleep(1)
+                print("Query started!")
+                await message.channel.send("Query started!")
+                await message.channel.send("Query sucessfully restarted!")
+
+        elif message.content == "s-Players" or message.content == "s-p" or message.content == "s-P":
+            if q == True and sOnline == True:
+                await message.channel.send(
+                    "The server has the following players online: {0}".format(", ".join(lastQuery.players.names)))
+            elif q == False:
+                await message.channel.send("Query is inactive at the moment. Ask the server owner to start it.")
+            elif sOnline == False:
+                await message.channel.send("The server is offline.")
+
+        elif message.content in ("s-Status", "s-s", "s-S"):
+            if q == True:
+                if sOnline == True:
+                    await message.channel.send("Server is online")
+                else:
+                    await message.channel.send("Server is offline")
+            else:
+                await message.channel.send("Query is inactive at the moment. Ask the server owner to start it.")
+
+        elif message.content in ("s-Version", "s-v", "s-V"):
+            if q == True:
+                if sOnline == True:
+                    await message.channel.send("The server is running version {0}".format(lastQuery.raw["version"]))
+                else:
+                    await message.channel.send("The server is offline.")
+            else:
+                await message.channel.send("Query is inactive at the moment. Ask the server owner to start it.")
+
+        elif message.content in ("s-motd", "s-m", "s-M"):
+            if q == True:
+                if sOnline == True:
+                    await message.channel.send("The message of the day is {0}".format(lastQuery.motd))
+                else:
+                    await message.channel.send("The server is offline.")
+            else:
+                await message.channel.send("Query is inactive at the moment. Ask the server owner to start it.")
+
+        elif message.content == "s-h" or message.content == "s-help":
+            await message.channel.send(
+                "Admin:\n**s-stopQuery** *stops the query*\n**s-startQuery** *starts the query*\n**s-restartQuery** *restarts the query*\n\nUser:\n**s-Players (s-p, s-P)** *lists players on server*\n**s-Status (s-s, s-S)** *checks if server is online*\n**s-Version (s-v, s-V)** *returns the version number the server is running*\n**s-motd (s-m, s-M)** *returns the message of the day/description of the server*\n**s-h (s-help)** *shows this list*")
+
 
 global q
 q = True
 
-global s
-s = ''
-
-global query
-global lastquery
-
 global server
-
 serverAdress = "000.000.000.000"    #replace with own IP adress or domain
 serverPort = 25565                  #replace with querry port of server
 
 server = MinecraftServer(serverAdress, serverPort)
 
+global lastQuery
+lastQuery = None
+
+global sOnline
+sOnline = False
 
 
-global Client
-Client = discord.Client()  # Initialise Client
-global client
-client = commands.Bot(command_prefix="?")  # Initialise client bot
-
-global channel
-global start
-
-
-@client.event
-async def on_ready():
-    global Client
-    global client
-
+def queryThread():
     global q
-    global query
-    global lastquery
-
-    global _loop
-
-    global channel
-    channel = client.get_channel('000000000000000000')  #replace with channel id of channel the bot should write to
-
-    print("Bot is online and connected to Discord")  # This will be called when the bot connects to the server
-    await client.send_message(channel, "Bot is online")
-
-    query = server.query()
-    lastquery = query.players.names
-
-    status = server.status()
-
-    print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
-    print("The server has the following players online: {0}".format(", ".join(query.players.names)))
-
-    _loop = asyncio.get_event_loop()
-
-    t.start()
-
-@client.event
-async def on_message(message):
-	global Client
-	global client
-
-	global q
-
-
-	global channel
-
-	if message.content == "cookie":
-		await client.send_message(message.channel, ":cookie:") #responds with Cookie emoji when someone says "cookie"
-#	elif message.content == "!start!":
-#		if message.author.id == "000000000000000000":   #replace with discord user ID of admin
-#			channel = message.channel
-#			start = message
-#			print("Query channel changed!")
-#			await client.send_message(channel, "Query started!")
-
-	elif message.content == "stopQuery":
-		if message.author.id == "000000000000000000":   #replace with discord user ID of admin
-			q = False
-
-			print("Query stoped!")
-			await client.send_message(channel, "Query stoped!")
-
-	elif message.content == "startQuery":
-		if message.author.id == "000000000000000000":   #replace with discord user ID of admin
-			q = True
-			t = Thread(target=querying)
-			t.start()
-
-			print("Query started!")
-			await client.send_message(channel, "Query started!")
-
-
-def querying():
-    global q
-    global query
-    global lastquery
-
     global server
-
-    global channel
-
-    global Client
-    global client
-
+    global lastQuery
+    global sOnline
+    global SSchannel
     global _loop
 
     while q:
-        # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
-        # status = server.status()
-        query = server.query()
-        if lastquery != query.players.names:
-            # print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
+        try:
+            server.query()
 
-            # 'query' has to be enabled in a servers' server.properties file.
-            # It may give more information than a ping, such as a full player list or mod information.
+        except:
+            if sOnline != False:
+                asyncio.run_coroutine_threadsafe(SSchannel.send("Server is offline."), _loop)
+                sOnline = False
+                lastQuery = None
+
+        else:
             query = server.query()
-            print("The server has the following players online: {0}".format(", ".join(query.players.names)))
+            lastQuery = query
 
-            lastquery = query.players.names
-
-            asyncio.run_coroutine_threadsafe(client.send_message(channel,
-                                                                 "The server has the following players online: {0}".format(
-                                                                     ", ".join(query.players.names))), _loop)
-
-        time.sleep(30)
+            if sOnline != True:
+                asyncio.run_coroutine_threadsafe(SSchannel.send("Server is online."), _loop)
+                sOnline = True
 
 
-t = Thread(target=querying)
+time.sleep(30)
+
+qt = Thread(target=queryThread)
 
 client.run("xxxxxxxxxxxxxxxxxxxxxxxx.xxxxxx.xxxxxxxxx-xxxxxxxxxxxxxxxxx")  # Replace token with your bots token
